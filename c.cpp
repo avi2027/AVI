cae7a332096753bb82db5dd35c7cdb0c4f4e636e
AVI MONDAL <avimondal@AVIs-MacBook-Air.local>
2023-05-04 03:36:53 +0600

    A

---
+++ c.cpp
@@ -0,0 +1,50 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <vector>
+#include <map>
+#include <set>
+#include <algorithm>
+#include <cstdlib>
+#include <cstdio>
+#include <cmath>
+#include <cstring>
+#include <cassert>
+#include <cctype>
+#include <ctime>
+#include <queue>
+#include <stack>
+#include <list>
+#include <iomanip>
+#include <bitset>
+#include <sstream>
+#include <fstream>
+#include <numeric>
+#include <functional>
+#include <utility>
+#include <iterator>
+#include <memory>
+#include <stdexcept>
+#include <functional>
+#include <limits>
+#include <random>
+#include <chrono>
+#include <ctime>
+#include <cstdlib>
+#include <cstdio>
+#include <cmath>
+#include <cstring>
+#include <cassert>
+#include <cctype>
+#include <ctime>
+#include <cstdlib>
+using namespace std;
+
+int add(int a, int b) {
+    return a + b;
+}
+
+int main() {
+    cout << add(1, 2) << endl;
+    return 0;
+}
