d2f76ce8a0f8e97a1b2acc627512391bdb9ebaba
AVI MONDAL <avimondal@AVIs-MacBook-Air.local>
2023-05-04 03:39:03 +0600

    First Commit

---
+++ cp.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int a,b;
+        printf("Enter The Two Numbers:");
+        scanf("%d %d",&a,&b);
+        printf("The MODULUS Value Is:%d",a%b);
+}
